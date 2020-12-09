a2ebf8eaaef7284dc84b55756a819323b9c3d6e6
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-12-08 22:49:21 -0500

    starting to add matching function

---
+++ commands/matches.js
@@ -0,0 +1,13 @@
+module.exports = {
+name: 'match',
+description: 'find out your relationship with other members in this server',
+execute(msg, args) {
+
+  const choices = [
+    'best friends',
+    'natural enemies',
+    
+  ]
+ 
+}
+}
